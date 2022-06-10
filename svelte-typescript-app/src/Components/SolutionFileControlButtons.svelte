7aa62b7b06711e404f5069e195a4eef359144482
Hunter Freeman <huntercfreeman@gmail.com>
2022-06-10 19:06:21 -0400

    Create a named .sln file

--- svelte-typescript-app/src/Components/SolutionFileControlButtons.svelte
+++ svelte-typescript-app/src/Components/SolutionFileControlButtons.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
 	import { MessageCreateDotNetSolutionInWorkspace } from "../../../out/Messages/Create/MessageCreateDotNetSolutionInWorkspace";
+    import TextInputForm from "./TextInputForm.svelte";
 
     export let getSolutionFilesInWorkspace;
 
     let isExpanded = false;
 
+    let addSolutionWithSpecifiedName: string | undefined;
+
     function createNewSln() {
-		let messageCreateDotNetSolutionInWorkspace = new MessageCreateDotNetSolutionInWorkspace();
+        let trimmedInput = addSolutionWithSpecifiedName.trim();
+        addSolutionWithSpecifiedName = undefined;
+
+		let messageCreateDotNetSolutionInWorkspace = new MessageCreateDotNetSolutionInWorkspace(trimmedInput);
 
 		tsVscode.postMessage({
 			type: undefined,
 			value: messageCreateDotNetSolutionInWorkspace
 		});
+	}
+    
+    function startNewSlnForm() {
+		addSolutionWithSpecifiedName = "";
 	}
 </script>
 
@@ -20,9 +30,20 @@
     <input type="checkbox" bind:checked="{isExpanded}" />
 
     {#if isExpanded}
-        <button on:click={createNewSln} style="margin-bottom: 5px;">
-            New Sln
-        </button>
+        <div>
+            <button on:click={startNewSlnForm} style="margin-bottom: 5px;">
+                New Sln
+            </button>
+            
+            {#if addSolutionWithSpecifiedName !== undefined}
+                <button on:click={createNewSln}>
+                    Opt for Default Solution Name
+                </button>
+                <TextInputForm bind:value="{addSolutionWithSpecifiedName}"
+                        onValidSubmit="{createNewSln}"
+                        placeholder="Solution name no extension" />
+            {/if}
+        </div>
 
         <button on:click={getSolutionFilesInWorkspace} style="margin-bottom: 5px;">
             Reload Solutions In Workspace
