53a0a73d419f865362598ac6147780912675a7da
Hunter Freeman <huntercfreeman@gmail.com>
2022-06-14 10:55:12 -0400

    Add quotes around command line absolute file paths in case filenames with a space

--- src/Constants/ConstantsDotNetCli.ts
+++ src/Constants/ConstantsDotNetCli.ts
@@ -6,7 +6,7 @@ import { DotNetSolutionFile } from "../FileSystem/Files/DotNetSolutionFile";
 /* eslint-disable @typescript-eslint/naming-convention */
 export class ConstantsDotNetCli {
     public static formatDotNetRunCSharpProject(cSharpProjectAbsoluteFilePath: AbsoluteFilePath): string {
-        return `dotnet run --project ${cSharpProjectAbsoluteFilePath.initialAbsoluteFilePathStringInput}`;
+        return `dotnet run --project \"${cSharpProjectAbsoluteFilePath.initialAbsoluteFilePathStringInput}\"`;
     }
     
     public static formatDotNetNewSolution(solutionNameWithoutExtension: string): string {
@@ -25,34 +25,34 @@ export class ConstantsDotNetCli {
     
     public static formatDotNetAddCSharpProjectToSolutionUsingProjectName(projectNameWithoutExtension: string, dotNetSolutionFile: DotNetSolutionFile): string {
 
-        return `dotnet sln ${dotNetSolutionFile.absoluteFilePath.initialAbsoluteFilePathStringInput} add ${projectNameWithoutExtension}/${projectNameWithoutExtension}.csproj`;
+        return `dotnet sln \"${dotNetSolutionFile.absoluteFilePath.initialAbsoluteFilePathStringInput}\" add ${projectNameWithoutExtension}/${projectNameWithoutExtension}.csproj`;
     }
     
     public static formatDotNetAddCSharpProjectToSolutionUsingProjectFsPath(projectFsPath: string, dotNetSolutionFile: DotNetSolutionFile): string {
 
-        return `dotnet sln ${dotNetSolutionFile.absoluteFilePath.initialAbsoluteFilePathStringInput} add ${projectFsPath}`;
+        return `dotnet sln \"${dotNetSolutionFile.absoluteFilePath.initialAbsoluteFilePathStringInput}\" add ${projectFsPath}`;
     }
     
     public static formatDotNetAddCSharpProjectReferenceToCSharpProject(receivingProjectAbsoluteFilePath: AbsoluteFilePath, referenceProjectAbsoluteFilePathString: string): string {
 
-        return `dotnet add ${receivingProjectAbsoluteFilePath.initialAbsoluteFilePathStringInput} reference ${referenceProjectAbsoluteFilePathString}`;
+        return `dotnet add \"${receivingProjectAbsoluteFilePath.initialAbsoluteFilePathStringInput}\" reference ${referenceProjectAbsoluteFilePathString}`;
     }
     
     public static formatDotNetRemoveCSharpProjectReferenceFromCSharpProject(parentCSharpProjectInitialAbsoluteFilePath: AbsoluteFilePath, cSharpProjectReferenceAbsoluteFilePath: AbsoluteFilePath): string {
 
-        return `dotnet remove ${parentCSharpProjectInitialAbsoluteFilePath.initialAbsoluteFilePathStringInput} reference ${cSharpProjectReferenceAbsoluteFilePath.initialAbsoluteFilePathStringInput}`;
+        return `dotnet remove \"${parentCSharpProjectInitialAbsoluteFilePath.initialAbsoluteFilePathStringInput}\" reference ${cSharpProjectReferenceAbsoluteFilePath.initialAbsoluteFilePathStringInput}`;
     }
     
     public static formatDotNetAddNugetPackageReferenceToCSharpProject(cSharpProjectInitialAbsoluteFilePath: AbsoluteFilePath, 
         nugetPackageModel: NugetPackageModel,
         nugetPackageVersionModel: NugetPackageVersionModel): string {
 
-        return `dotnet add ${cSharpProjectInitialAbsoluteFilePath.initialAbsoluteFilePathStringInput} package ${nugetPackageModel.id} --version ${nugetPackageVersionModel.version}`;
+        return `dotnet add \"${cSharpProjectInitialAbsoluteFilePath.initialAbsoluteFilePathStringInput}\" package ${nugetPackageModel.id} --version ${nugetPackageVersionModel.version}`;
     }
     
     public static formatDotNetRemoveNugetPackageReferenceFromCSharpProject(cSharpProjectInitialAbsoluteFilePath: AbsoluteFilePath, nugetPackageId: string): string {
 
-        return `dotnet remove ${cSharpProjectInitialAbsoluteFilePath.initialAbsoluteFilePathStringInput} package ${nugetPackageId}`;
+        return `dotnet remove \"${cSharpProjectInitialAbsoluteFilePath.initialAbsoluteFilePathStringInput}\" package ${nugetPackageId}`;
     }
     
     public static DOT_NET_NEW_LIST = "dotnet new --list";
