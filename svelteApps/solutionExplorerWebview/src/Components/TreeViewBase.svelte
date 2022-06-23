9a1e5cd0bdfbf38b59cec920bf2300cff069e612
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-06-23 16:11:10 -0400

    Progress on ArrowUp

--- svelteApps/solutionExplorerWebview/src/Components/TreeViewBase.svelte
+++ svelteApps/solutionExplorerWebview/src/Components/TreeViewBase.svelte
@@ -5,8 +5,8 @@
 	import FileIconDisplay from "./FileIconDisplay.svelte";
 	import { contextMenuTarget } from "./menu.js";
 	import TreeViewMapper from "./TreeViewMapper.svelte";
-	import { activeIdeFileWrap, ActiveIdeFileWrapTuple } from "./activeState"
-	import { activeIdeFileHandleOnKeyDownWrap } from "./activeState"
+	import { activeIdeFileWrap, ActiveIdeFileWrapTuple } from "./activeState";
+	import { activeIdeFileHandleOnKeyDownWrap } from "./activeState";
 
 	export let ideFile: IdeFile;
 	export let children: IdeFile[] | undefined;
@@ -17,11 +17,10 @@
 	export let index: number;
 	export let parent: IdeFile;
 	export let parentChildren: IdeFile[];
-	
+
 	let activeIdeFileWrapValue;
 
 	activeIdeFileWrap.subscribe((value) => {
-
 		if (!value) {
 			return;
 		}
@@ -31,15 +30,27 @@
 		activeIdeFileWrapValue = activeIdeFileWrapTuple.ideFile;
 
 		// If this IdeFile is the activeIdeFile it will handle key down events.
-		if (activeIdeFileWrapTuple.ideFile && ((activeIdeFileWrapTuple.ideFile).nonce === ideFile.nonce)) {
-
+		if (
+			activeIdeFileWrapTuple.ideFile &&
+			activeIdeFileWrapTuple.ideFile.nonce === ideFile.nonce
+		) {
 			// Ignore key down events until completely finished handling previous key down event
 			activeIdeFileHandleOnKeyDownWrap.set(undefined);
 
 			// Finish handling previous key down event that requires more than 1 movement operation
 			if (activeIdeFileWrapTuple.callbacks?.length > 0) {
-				for (let i = 0; i < activeIdeFileWrapTuple.callbacks.length; i++) {
-					activeIdeFileWrapTuple.callbacks[i](ideFile, index, parent, parentChildren);
+				for (
+					let i = 0;
+					i < activeIdeFileWrapTuple.callbacks.length;
+					i++
+				) {
+					activeIdeFileWrapTuple.callbacks[i](
+						ideFile,
+						index,
+						children,
+						parent,
+						parentChildren
+					);
 				}
 
 				// More than 1 movement operation likely means this cannot accurrately be assummed as the active ide file
@@ -50,12 +61,11 @@
 			activeIdeFileHandleOnKeyDownWrap.set(handleOnKeyDown);
 		}
 	});
-	
+
 	$: activeIdeFile = activeIdeFileWrapValue as IdeFile;
 
-	$: isActiveCssClass = (activeIdeFile?.nonce ?? "") === ideFile.nonce
-		? "dni_active"
-		: "";
+	$: isActiveCssClass =
+		(activeIdeFile?.nonce ?? "") === ideFile.nonce ? "dni_active" : "";
 
 	let contextMenuTargetValue;
 
@@ -63,24 +73,22 @@
 		contextMenuTargetValue = value;
 	});
 
-	$: isActiveContextMenuTarget = ((contextMenuTargetValue as IdeFile)?.nonce ?? "") === ideFile.nonce
-		? "dni_active-context-menu-target"
-		: "";
+	$: isActiveContextMenuTarget =
+		((contextMenuTargetValue as IdeFile)?.nonce ?? "") === ideFile.nonce
+			? "dni_active-context-menu-target"
+			: "";
 
 	function fireTitleOnDoubleClick(e: MouseEvent) {
-
 		setActiveIdeFileAsSelf();
 
 		titleOnClick(e);
 	}
-	
-	function setActiveIdeFileAsSelf() {
 
+	function setActiveIdeFileAsSelf() {
 		activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(ideFile, undefined));
 	}
-	
-	function setActiveIdeFileAsParent() {
 
+	function setActiveIdeFileAsParent() {
 		activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(parent, undefined));
 	}
 
@@ -88,82 +96,124 @@
 		if (e.key === ConstantsKeyboard.KEY_ARROW_LEFT) {
 			if (ideFile.isExpanded) {
 				ideFile.isExpanded = false;
-			}
-			else if (parent) {
+			} else if (parent) {
 				setActiveIdeFileAsParent();
 			}
-		}
-		else if (e.key === ConstantsKeyboard.KEY_ARROW_RIGHT) {
+		} else if (e.key === ConstantsKeyboard.KEY_ARROW_RIGHT) {
 			if (!ideFile.isExpanded) {
 				ideFile.isExpanded = true;
+			} else if ((children?.length ?? 0) > 0) {
+				activeIdeFileWrap.set(
+					new ActiveIdeFileWrapTuple(children[0], undefined)
+				);
 			}
-			else if ((children?.length ?? 0) > 0) {
-				activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(children[0], undefined));
+		} else if (e.key === ConstantsKeyboard.KEY_ARROW_DOWN) {
+			if (ideFile.isExpanded && (children?.length ?? 0) > 0) {
+				activeIdeFileWrap.set(
+					new ActiveIdeFileWrapTuple(children[0], undefined)
+				);
+			} else if (parentChildren && parentChildren.length > index + 1) {
+				activeIdeFileWrap.set(
+					new ActiveIdeFileWrapTuple(
+						parentChildren[index + 1],
+						undefined
+					)
+				);
+			} else if (parent) {
+				activeIdeFileWrap.set(
+					new ActiveIdeFileWrapTuple(parent, [
+						(
+							upperIdeFile,
+							upperIndex,
+							upperChildren,
+							upperParent,
+							upperParentChildren
+						) => {
+							if (
+								upperParentChildren &&
+								upperParentChildren.length > upperIndex + 1
+							) {
+								activeIdeFileWrap.set(
+									new ActiveIdeFileWrapTuple(
+										upperParentChildren[upperIndex + 1],
+										undefined
+									)
+								);
+							}
+						},
+					])
+				);
 			}
-		}
-		else if (e.key === ConstantsKeyboard.KEY_ARROW_DOWN) {
-			if (ideFile.isExpanded && ((children?.length ?? 0) > 0)) {
-				activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(children[0], undefined));
-			}
-			else if (parentChildren && (parentChildren.length > (index + 1))) {
-				activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(parentChildren[index + 1], undefined));
-			}
-			else if (parent) {
-				activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(parent, [
-					/*
-						In short, key down in some cases needs to set active
-						a sibling of the parent. The callbacks allow for that.
+		} else if (e.key === ConstantsKeyboard.KEY_ARROW_UP) {
+			if (parent && parentChildren && index > 0) {
+				let siblingFile = parentChildren[index - 1];
 
-						upperIdeFile THIS ideFile's parent
-						upperIndex THIS ideFile's parent's index
-						upperParent THIS ideFile's parent's parent
-						upperParentChildren THIS ideFile's parent's parent's children
-					 */
-					(upperIdeFile, upperIndex, upperParent, upperParentChildren) => {
-						if (upperParentChildren && (upperParentChildren.length > (upperIndex + 1))) {
-							activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(upperParentChildren[upperIndex + 1], undefined));
+				if (siblingFile.isExpanded) {
+					function recursivelyGetActiveIdeFile(
+						childIdeFile: IdeFile,
+						childFileIndex: number,
+						childFileChildren: IdeFile[] | undefined,
+						childFileParent: IdeFile | undefined,
+						childFileParentChildren: IdeFile[] | undefined
+					): void {
+						if (
+							childIdeFile.isExpanded &&
+							childFileChildren &&
+							childFileChildren.length > 0
+						) {
+							let lastChild =
+								childFileChildren[childFileChildren.length - 1];
+
+							activeIdeFileWrap.set(
+								new ActiveIdeFileWrapTuple(lastChild, [
+									recursivelyGetActiveIdeFile,
+								])
+							);
+						}
+						else {
+							activeIdeFileWrap.set(
+								new ActiveIdeFileWrapTuple(childIdeFile, undefined)
+							);
 						}
 					}
-				]));
-			}
-		}
-		else if (e.key === ConstantsKeyboard.KEY_ARROW_UP) {
-					console.log("ConstantsKeyboard.KEY_ARROW_UP");
-			if (parent && parentChildren && (index > 0)) {
 
-				let currentIdeFile = parentChildren[index - 1];
+					activeIdeFileWrap.set(
+						new ActiveIdeFileWrapTuple(siblingFile, [
+							(
+								siblingIdeFile,
+								siblingFileIndex,
+								siblingFileChildren,
+								siblingFileParent,
+								siblingFileParentChildren
+							) => {
+								if (
+									siblingFileChildren &&
+									siblingFileChildren.length > 0
+								) {
+									let lastChild =
+										siblingFileChildren[
+											siblingFileChildren.length - 1
+										];
 
-				while (currentIdeFile.isExpanded) {
-
-					console.log("while (currentIdeFile.isExpanded) {")
-
-					let currentIdeFileChildren = (currentIdeFile.constantChildFiles ?? [])
-						.concat((currentIdeFile.childFiles ?? []))
-						.concat((currentIdeFile.virtualChildFiles ?? []));
-
-					if ((currentIdeFileChildren?.length ?? 0) <= 0) {
-						break;
+									activeIdeFileWrap.set(
+										new ActiveIdeFileWrapTuple(
+											siblingFileChildren[lastChild],
+											[recursivelyGetActiveIdeFile]
+										)
+									);
+								}
+								else {
+									activeIdeFileWrap.set(
+										new ActiveIdeFileWrapTuple(siblingIdeFile, undefined)
+									);
+								}
+							},
+						])
+					);
+				} else {
+					if (parent) {
+						setActiveIdeFileAsParent();
 					}
-
-					let indexToGetChild = currentIdeFileChildren.length - 1;
-					
-					currentIdeFile = currentIdeFileChildren[indexToGetChild--];
-
-					while (currentIdeFile.hasUnexpectedParent && (indexToGetChild !== -1)) {
-						currentIdeFile = currentIdeFileChildren[indexToGetChild--];
-					}
-
-					console.log("while (currentIdeFile.hasUnexpectedParent && (indexToGetChild !== -1)) {")
-					if (indexToGetChild === -1) {
-						break;
-					}
-				}
-
-				activeIdeFileWrap.set(new ActiveIdeFileWrapTuple(currentIdeFile, undefined));
-			}
-			else {
-				if (parent) {
-					setActiveIdeFileAsParent();
 				}
 			}
 		}
@@ -184,18 +234,22 @@
 				tabindex="-1"
 				class="dni_do-not-show-chevron"
 			>
-				<ExpansionChevron isExpanded={false}
-				                  onClickAction={setActiveIdeFileAsSelf} />
+				<ExpansionChevron
+					isExpanded={false}
+					onClickAction={setActiveIdeFileAsSelf}
+				/>
 			</span>
 		{:else}
 			<span class="dni_tree-view-title-expansion-chevron">
-				<ExpansionChevron bind:isExpanded={ideFile.isExpanded}
-				                  onClickAction={setActiveIdeFileAsSelf} />
+				<ExpansionChevron
+					bind:isExpanded={ideFile.isExpanded}
+					onClickAction={setActiveIdeFileAsSelf}
+				/>
 			</span>
 		{/if}
 
 		<span class="dni_tree-view-title-text">
-			<FileIconDisplay ideFile={ideFile} />
+			<FileIconDisplay {ideFile} />
 
 			{index + "_" + titleText}
 		</span>
@@ -205,7 +259,12 @@
 		{#if ideFile.isExpanded}
 			{#each children ?? getChildFiles() ?? [] as child, i (child.nonce)}
 				{#if !hasDifferentParentContainer(child)}
-					<TreeViewMapper ideFile={child} index={i} parent={ideFile} parentChildren={children} />
+					<TreeViewMapper
+						ideFile={child}
+						index={i}
+						parent={ideFile}
+						parentChildren={children}
+					/>
 				{/if}
 			{/each}
 		{/if}
@@ -246,7 +305,7 @@
 	.dni_tree-view-title.dni_active-context-menu-target {
 		border: 1px solid var(--vscode-focusBorder);
 	}
-	
+
 	/* ....when extensionactive.... .dni_tree-view-title.dni_active {
 		background-color: var(--vscode-list-inactiveSelectionBackground);
 	} */
--- svelteApps/solutionExplorerWebview/src/Components/activeState.js
+++ svelteApps/solutionExplorerWebview/src/Components/activeState.js
@@ -3,7 +3,20 @@ import { writable } from 'svelte/store';
 export const activeIdeFileWrap = writable(undefined);
 export const activeIdeFileHandleOnKeyDownWrap = writable(undefined);
 
-// callbacks: ((ideFile, index, parentIdeFile, parentIdeFileChildren) => void)[] | undefined
+/**
+The callbacks are used to:
+
+- Traverse parent's siblings
+- Traverse sibling's children
+
+---
+
+- ArrowDown when there are no siblings below needs to traverse to the next sibling of the parent.
+
+- ArrowUp when the sibling above is expanded needs to recursively traverse sibling's children.
+
+callbacks: ((ideFile, index, children, parentIdeFile, parentIdeFileChildren) => void)[] | undefined
+ */
 export class ActiveIdeFileWrapTuple {
     constructor(ideFile, callbacks) {
         this.ideFile = ideFile;
